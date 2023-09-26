5882306aa2baa5c885e69aae381380a0f98bd28b
Sachin Subugade <sachinrs1310@gmail.com>
2023-09-26 22:08:12 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text} from 'react-native';
+import { t } from 'react-native-tailwindcss';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Deliveroo Clone</Text>
+    </View>
+  );
+};
+
+export default App;
